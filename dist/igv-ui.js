cb782886cf55bc2c7ad3d4350784c9405ffc664c
jrobinso <jrobinso@broadinstitute.org>
2021-05-12 09:54:34 -0700

    update igv-utils dep

--- dist/igv-ui.js
+++ dist/igv-ui.js
@@ -5528,12 +5528,28 @@ Zlib.Deflate.prototype.compress = function() {
   return output;
 };
 
-let _btoa;
-if(typeof btoa === 'undefined') {
-    _btoa = require('btoa');
-} else {
-    _btoa = btoa;
-}
+//	%23	%24	%25	%26	%27	%28	%29	%2A	%2B	%2C	%2F	%3A	%3B	%3D	%3F	%40	%5B	%5D
+const encodings = new Map();
+encodings.set("!", "%21");
+encodings.set("#", "%23");
+encodings.set("$", "%24");
+encodings.set("%", "%25");
+encodings.set("&", "%26");
+encodings.set("'", "%27");
+encodings.set("(", "%28");
+encodings.set(")", "%29");
+encodings.set("*", "%2A");
+encodings.set("+", "%2B");
+encodings.set(",", "%2C");
+encodings.set("/", "%2F");
+encodings.set(":", "%3A");
+encodings.set(";", "%3B");
+encodings.set("=", "%3D");
+encodings.set("?", "%3F");
+encodings.set("@", "%40");
+encodings.set("[", "%5B");
+encodings.set("]", "%5D");
+encodings.set(" ", "%20");
 
 if (typeof process === 'object' && typeof window === 'undefined') {
     global.atob = function (str) {
